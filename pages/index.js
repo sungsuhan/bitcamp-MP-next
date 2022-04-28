e843afcda925e939885b77baf3ad54c16b662b40
sungsuhan <swhanssu@gmail.com>
2022-04-28 14:26:29 +0900

    2022-04-28 14:26 first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { Footer, Layout, Pagination, Nav, Table, Modal } from '@/components'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head/>
+      <Table/>
+      <Pagination/>
+      <Modal/>
+    </Layout>
+  )
+}
